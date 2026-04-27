83e0ca8e21cff9a15c225aa7b892e766787897a9
Dev-Ganesh-Sable <dev.sableganesh0612@gmail.com>
2026-04-27 13:23:00 +0530

    add some code in demo file

--- all-topic/src/Component/Demo.js
+++ all-topic/src/Component/Demo.js
@@ -1,2 +1,12 @@
 
-<h1>Welcome to React</h1>
+import React from 'react'
+
+function Demo() {
+    return (
+        <div>
+            <h1>Welcome to React</h1>
+        </div>
+    )
+}
+
+export default Demo
