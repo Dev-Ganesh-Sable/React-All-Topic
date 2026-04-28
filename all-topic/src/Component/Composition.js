660e9952660ee9c7b36736d9910ccd960ff836b5
Dev-Ganesh-Sable <dev.sableganesh0612@gmail.com>
2026-04-28 16:41:31 +0530

    add composition file and rendering folder

---
+++ all-topic/src/Component/Composition.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// composition : Composition in React means building complex components by combining smaller, reusable components.
+
+// Instead of inheritance, React uses composition to reuse code
+
+function Composition() {
+  return (
+    <div>Composition</div>
+  )
+}
+
+export default Composition
