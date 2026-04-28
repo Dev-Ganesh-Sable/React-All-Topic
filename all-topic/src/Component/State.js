ede83a99f06520acd693024b461da5b16dc47082
Dev-Ganesh-Sable <dev.sableganesh0612@gmail.com>
2026-04-28 16:15:26 +0530

    add some theory part in state.js file

--- all-topic/src/Component/State.js
+++ all-topic/src/Component/State.js
@@ -4,6 +4,11 @@ import React, { useState } from 'react'
 
 // State = data that can change over time and re-render the UI
 
+// how its work  :
+// count → current state value
+// setCount → function to update state
+// When state changes → component re-renders
+
 function State() {
 
     const [count, setCount] = useState(0);
@@ -15,8 +20,6 @@ function State() {
             <button onClick={() => setCount(count - 1)}>Decrement Count</button>
             <button onClick={() => setCount(count + 5)}>Increment Count BY 5 </button>
             <button onClick={() => setCount(count - 5)}>Decrement Count BY 5 </button>
-
-
 
         </div>
     )
